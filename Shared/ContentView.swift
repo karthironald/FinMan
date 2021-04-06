3d97ebc8fca4d60cd1f648de7c0d9e05476f1d13
Karthick Selvaraj <karthick@mallow-tech.com>
2021-04-06 22:00:10 +0530

    Initial commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Karthick Selvaraj on 06/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello World")
+                .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/FinManApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinManApp.swift
+//  Shared
+//
+//  Created by Karthick Selvaraj on 06/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FinManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
